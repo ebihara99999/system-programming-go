b6e8c02cc242c89e70b5d6d87f37d3cf1f35ece5
Yusuke Ebihara <dev.biibiebi@gmail.com>
2018-09-29 20:36:34 +0900

    Receive input from stdin ( the most simple program )

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"bufio"
-	"os"
+	"fmt"
+	"io"
 )
 
 func main() {
-	buffer := bufio.NewWriter(os.Stdout)
-	buffer.WriteString("bufio.Writer ")
-	buffer.Flush()
-	buffer.WriteString("example\n")
-	buffer.Flush()
+	for {
+		buffer := make([]byte, 5)
+		size, err := os.Stdin.Read(buffer)
+		if err == io.EOF {
+			fmt.Println("EOF")
+			break
+		}
+		fmt.Printf("size=%d input='%s'\n", size, string(buffer))
+	}
 }
