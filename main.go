5ba8b3aaefd1727a2fd26a5515c2ac08f4a7fd21
Yusuke Ebihara <dev.biibiebi@gmail.com>
2018-09-29 21:37:30 +0900

    Use net.Conn

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"io"
+	"net"
+	"net/http"
 	"os"
 )
 
 func main() {
-	file, err := os.Open("file.go")
+	conn, err := net.Dial("tcp", "ascii.jp:80")
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
-	io.Copy(os.Stdout, file)
+
+	conn.Write([]byte("GET / HTTP/1.1\r\nHost: ascii.jp\r\n\r\n"))
+	res, err := http.ReadResponse(bufio.NewReader(conn), nil)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	io.Copy(os.Stdout, res.Body)
 }
